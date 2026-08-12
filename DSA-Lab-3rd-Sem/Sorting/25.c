e488495bfbe30bfbf401844edb98e7b7c302e80f
d1vyom <divyomkumardk2007@gmail.com>
2026-08-13 02:31:21 +0530

    Merge Sort

---
+++ DSA-Lab-3rd-Sem/Sorting/25.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSortRecursive(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSortRecursive(arr, l, m);
+        mergeSortRecursive(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+int mergeSort(int arr[], int n) {
+    mergeSortRecursive(arr, 0, n - 1);
+    return 0;
+}
+
+int main() {
+    int n, i;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+
+    int ans[n];
+    printf("Enter elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &ans[i]);
+    }
+    mergeSort(ans, n);
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", ans[i]);
+    }
+    printf("\n");
+    return 0;
+}
